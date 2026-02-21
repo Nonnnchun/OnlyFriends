4026b875c6ba25129ef076a3f0a98af508c05de7
Archer <superkakanoob@gmail.com>
2026-02-21 14:17:25 +0700

    Working on connecting Event detail frontend with API

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using OnlyFriends.Models;
+using OnlyFriends.ApiControllers;
 
 namespace OnlyFriends.Controllers
 {
     public class EventController : Controller
     {
+         [Route("/event/view/{id}")]
         public IActionResult EventDetails()
     {
        
