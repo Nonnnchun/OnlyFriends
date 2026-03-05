8d71cc8cf5abfe848367e82dfc591cb7069f0c02
Nonnnchern <67011509@kmitl.ac.th>
2026-03-05 15:58:33 +0700

    fucking obj

--- obj/Debug/net10.0/onlyfriends.AssemblyInfo.cs
+++ obj/Debug/net10.0/onlyfriends.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("onlyfriends")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d7c0de7429d6b900f3d7ce4c97589ae850fcc8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e4cac32615431b78493492498acb7619dcd62a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("onlyfriends")]
 [assembly: System.Reflection.AssemblyTitleAttribute("onlyfriends")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
