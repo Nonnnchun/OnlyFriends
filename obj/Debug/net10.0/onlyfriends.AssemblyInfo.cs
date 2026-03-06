6c947ddb62340d9de3c4db5b1572f59311278be2
Yokphon Ninbarun <yokphon.work@gmail.com>
2026-03-07 00:55:53 +0700

    Project 80%

--- obj/Debug/net10.0/onlyfriends.AssemblyInfo.cs
+++ obj/Debug/net10.0/onlyfriends.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("onlyfriends")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cc210201718717040e687cb3f20babcd7ae8644")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d10da141500831bf624116cfebf42f94224af7fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("onlyfriends")]
 [assembly: System.Reflection.AssemblyTitleAttribute("onlyfriends")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
