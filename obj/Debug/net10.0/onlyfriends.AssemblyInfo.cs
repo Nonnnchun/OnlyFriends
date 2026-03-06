964a8888ec9aa9b74f25699c169b1f062b5026a0
Nonnnchern <150591629+Nonnnchun@users.noreply.github.com>
2026-03-07 01:51:04 +0700

    wait to merge dev

--- obj/Debug/net10.0/onlyfriends.AssemblyInfo.cs
+++ obj/Debug/net10.0/onlyfriends.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("onlyfriends")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d71cc8cf5abfe848367e82dfc591cb7069f0c02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b18be06950cfb1498fcca9de64b9c3ddf26f50e")]
 [assembly: System.Reflection.AssemblyProductAttribute("onlyfriends")]
 [assembly: System.Reflection.AssemblyTitleAttribute("onlyfriends")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
