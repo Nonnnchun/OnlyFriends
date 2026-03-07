55b7c23394dcfa6cf0623e5476e1e88d248c33cf
ppp-plm <sirapop2807@gmail.com>
2026-03-08 01:32:09 +0700

    ไปbranchpalm

--- obj/Debug/net10.0/onlyfriends.AssemblyInfo.cs
+++ obj/Debug/net10.0/onlyfriends.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("onlyfriends")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8736398cdfe6b0c313a151f3b2730be2e351e54a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95223c67949ab85270308cf27b1003043fa57c5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("onlyfriends")]
 [assembly: System.Reflection.AssemblyTitleAttribute("onlyfriends")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
