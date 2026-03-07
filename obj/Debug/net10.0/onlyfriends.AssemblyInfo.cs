a9a41a0a7f7f090345c22588e1f749f859843517
ppp-plm <sirapop2807@gmail.com>
2026-03-08 01:33:47 +0700

    mergewithdev

--- obj/Debug/net10.0/onlyfriends.AssemblyInfo.cs
+++ obj/Debug/net10.0/onlyfriends.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("onlyfriends")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8921f813bf011852e32fd242f4f54a62a8e7cada")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+744e16f83d581cce23365df343d68a6e1ebd93a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("onlyfriends")]
 [assembly: System.Reflection.AssemblyTitleAttribute("onlyfriends")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
