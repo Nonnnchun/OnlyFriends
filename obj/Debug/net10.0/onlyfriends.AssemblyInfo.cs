9fc798d1155255ae7a46ebfc8021838fa3c0ef0a
Nonnnchern <150591629+Nonnnchun@users.noreply.github.com>
2026-03-11 21:43:00 +0700

    .page-loading

--- obj/Debug/net10.0/onlyfriends.AssemblyInfo.cs
+++ obj/Debug/net10.0/onlyfriends.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("onlyfriends")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a534e7bb94771cb13630d23c1bc3c91e01c0ab85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6873809bb71507617bed176af6e70b7564c4b14")]
 [assembly: System.Reflection.AssemblyProductAttribute("onlyfriends")]
 [assembly: System.Reflection.AssemblyTitleAttribute("onlyfriends")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
